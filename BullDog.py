52d702f030b6f72106168bc9cadab93a7e33ce3b
0x07CB <0x07CB@users.noreply.github.com>
2022-01-25 16:28:51 +0100

    first

---
+++ BullDog.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+#coding: utf-8
+
+watchlist = [ ] # files path .... to watch integrity
+
+algo_hash = "sha512"
+
+import hashlib 
+from base64 import b64encode as benc
+from base64 import b64decode as bdec
+
+
+
+
+class Dog(object):
+    def __init__(self):
+        pass
+        
+    def watch(self,filepath):
+        pass
+        
+    def whooaf(self,title,message):
+        pass
+        
+    def grrr(self,loglevel=1):
+        pass
+        
+    def storeCurrentBones(self):
+        pass
+        
+    def undoVersion(self):
+        pass
+        
+    def extractArchivedVersionsOf(self,filepath):
+        pass
