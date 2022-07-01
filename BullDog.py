5d4e86931b433e98aba9bcddc48d0a480d4aa2b8
0x07CB <83157348+0x07CB@users.noreply.github.com>
2022-07-01 23:09:50 +0200

    correction 1

--- BullDog.py
+++ BullDog.py
@@ -1,12 +1,12 @@
 #!/bin/python3
 #coding: utf-8
 
-# read config 
+# read config
 watchlist = [ ] # files path .... to watch integrity
 
 algo_hash = "sha512"
 
-import hashlib 
+import hashlib
 from base64 import b64encode as benc
 from base64 import b64decode as bdec
 
@@ -16,26 +16,26 @@ from base64 import b64decode as bdec
 class Dog(object):
     def __init__(self):
         self.logfilepath="grrr.log"
-        
+
     def watch(self,filepath):
         with open(filepath, "rb") as f:
             data = f.read()
             f.close()
         h = hashlib.sha3_512(data)
         return h.hexdigest().decode()
-        
+
     def whooaf(self,title,message):
         pass
-        
+
     def grrr(self):
         with open(self.logfilepath, "a") as f:
             f.write("{}\n".format(self.changes))
             f.close()
-        
+
     def storeCurrentBones(self):
         pass
-        
+
     def undoVersion(self):
         pass
-        
+
     def extract(self,file)
